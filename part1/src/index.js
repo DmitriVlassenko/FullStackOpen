1484023949fefae96f7379f0f649d78141b83cb4
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-02-21 21:28:53 +0200

    1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,36 +4,34 @@ import './index.css'
 
 const App = () => {
 
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
 
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
+const Header = ({course}) => (<h1>{course}</h1>)
 
 const Content = (props) => {
     return (
@@ -41,7 +39,6 @@ const Content = (props) => {
             <Part part={props.parts[0].exercises} ex={props.parts[0].name}/>
             <Part part={props.parts[1].exercises} ex={props.parts[1].name}/>
             <Part part={props.parts[2].exercises} ex={props.parts[2].name}/>
-
         </>
     )
 }
@@ -53,12 +50,6 @@ const Total = (props) => {
     )
 }
 
-const Part = (props) => {
-    return (
-        <>
-            <p>{props.part} {props.ex}</p>
-        </>
-    )
-}
+const Part = ({part, ex}) => (<p>{part} {ex}</p>)
 
 ReactDOM.render(<App />, document.getElementById('root'))
