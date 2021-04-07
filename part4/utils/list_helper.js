444fa6299af5f1191bf299046a518cdbf1aebf89
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-04-07 17:49:36 +0300

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
     return blogs
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, currentValue) => {
+        console.log(sum, currentValue.likes)
+        return sum + currentValue.likes
+    }, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
