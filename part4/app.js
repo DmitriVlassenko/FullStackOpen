b6c542efd2d6e3433071846a95c8216941562d37
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-04-13 14:59:42 +0300

    4.15: bloglist expansion, step3

--- part4/app.js
+++ part4/app.js
@@ -7,6 +7,7 @@ const blogRouter = require('./controllers/blogNotes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
