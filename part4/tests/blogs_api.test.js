544a8c19951820247ea4fdc824666c69671a670a
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-04-13 15:05:10 +0300

    4.16*: bloglist expansion, step4

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -125,6 +125,27 @@ describe('when there is initially one user in db', () => {
         const usersAtEnd = await helper.usersInDb()
         expect(usersAtEnd).toHaveLength(usersAtStart.length)
     })
+
+    test('creation fails with proper status code and message if username is short', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'ro',
+            name: 'Superuser',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('is shorter than the minimum allowed length')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
+    })
 })
 
 afterAll(() => {
