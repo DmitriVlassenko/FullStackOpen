f3e40e45f5613af509a0c7d7e55f49ae46caa63d
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-04-13 16:51:53 +0300

    4.20*: bloglist expansion, step8

--- part4/controllers/blogNotes.js
+++ part4/controllers/blogNotes.js
@@ -13,25 +13,11 @@ blogsRouter.get('/:id', async (request, response) => {
     response.json(blog)
 })
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.post('/', async (request, response) => {
-    // const blog = new Blog(request.body)
-    //
-    // const savedBlog = await blog.save()
-    // response.json(savedBlog)
-
     const body = request.body
 
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
