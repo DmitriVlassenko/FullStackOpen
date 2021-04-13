dfa1bdf9d4e4ce9ec7c0947d6aac26cbc75f25a7
Dmitri Vlassenko <dmitrivlassenko@Dmitris-MacBook-Air.local>
2021-04-13 17:56:35 +0300

    Part4done

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -25,52 +25,6 @@ describe('blogs api tests', () => {
         response.body.map(x => expect(x.id).toBeDefined())
     })
 
-    test('HTTP POST request to the /api/blogs url successfully creates a new blog post', async () => {
-        const newBlog = helper.initialBlogs[0]
-
-        await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-
-        const blogsAtEnd = await helper.blogsInDb()
-        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-        const title = blogsAtEnd.map(x => x.title)
-        expect(title).toContain('blog number 1 for testing')
-    })
-
-    test('if the likes property is missing from the request, it will default to the value 0', async () => {
-        const newBlog =
-            {
-                title: "blog number 3 for testing",
-                author: "Dmitri",
-                url: "test"
-            }
-
-        await api
-            .post('/api/blogs')
-            .send(newBlog)
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-
-        const blogs = await helper.blogsInDb()
-        const noLikes = await blogs.find(x => x.title === 'blog number 3 for testing')
-        expect(noLikes.likes).toEqual(0)
-    })
-
-    test('if the title and url are missing from the request, backend responds 400 Bad Request.', async () => {
-        const badPost = {
-            author: 'Dmitri',
-            likes: 0
-        }
-
-        await api
-            .post('/api/blogs')
-            .send(badPost)
-            .expect(400)
-    })
 })
 
 describe('when there is initially one user in db', () => {
